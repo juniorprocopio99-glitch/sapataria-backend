b637d7fe1b316b0f25c18ab6e617c0bad2e77b45
junior procopio <juniorprocopio99@gmail.com>
2026-05-22 13:44:44 -0300

    supabase conectado

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const supabase = createClient(
+  'COLE_A_URL_DO_SUPABASE',
+  'COLE_A_CHAVE_PUBLICAVEL'
+);
+
 app.get('/', (req, res) => {
   res.send('API funcionando');
+});
+
+app.get('/produtos', async (req, res) => {
+  const { data, error } = await supabase
+    .from('produtos')
+    .select('*');
+
+  if (error) {
+    return res.status(500).json(error);
+  }
+
+  res.json(data);
 });
 
 app.listen(3001, () => {
